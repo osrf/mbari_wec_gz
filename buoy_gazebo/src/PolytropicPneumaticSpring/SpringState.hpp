170d3a48f20b05c521d6af17f4bef910903b1fa8
Michael Anderson <anderson@mbari.org>
2022-05-05 20:40:01 -0700

    need to make it compile

---
+++ buoy_gazebo/src/PolytropicPneumaticSpring/SpringState.hpp
@@ -0,0 +1,38 @@
+// Copyright 2022 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef POLYTROPICPNEUMATICSPRING__SPRINGSTATE_HPP_
+#define POLYTROPICPNEUMATICSPRING__SPRINGSTATE_HPP_
+
+#include <ignition/gazebo/components/Factory.hh>
+#include <ignition/gazebo/components/Component.hh>
+#include <ignition/gazebo/config.hh>
+
+namespace buoy_gazebo
+{
+
+struct SpringState_
+{
+  int16_t load_cell{0};
+  float range_finder{0.0F};
+  float upper_psi{0.0F};
+  float lower_psi{0.0F};
+};
+
+using SpringState = ignition::gazebo::components::Component<SpringState_,
+    class SpringStateTag>;
+IGN_GAZEBO_REGISTER_COMPONENT("buoy_gazebo.SpringState", SpringState)
+}  // namespace buoy_gazebo
+
+#endif  // POLYTROPICPNEUMATICSPRING__SPRINGSTATE_HPP_
---
+++ buoy_gazebo/src/controllers/SpringController/SpringController.cpp
@@ -0,0 +1,145 @@
+#include "SpringController.hpp"
+#include "PolytropicPneumaticSpring/SpringState.hpp"
+
+#include <ignition/gazebo/components/Name.hh>
+#include <ignition/plugin/Register.hh>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <buoy_msgs/msg/sc_record.hpp>
+
+
+IGNITION_ADD_PLUGIN(
+    buoy_gazebo::SpringController,
+    ignition::gazebo::System,
+    buoy_gazebo::SpringController::ISystemConfigure,
+    buoy_gazebo::SpringController::ISystemPostUpdate)
+
+using namespace buoy_gazebo;
+
+
+struct SpringControllerPrivate
+{
+  ignition::gazebo::Entity entity_;
+  std::chrono::steady_clock::duration current_time_;
+  rclcpp::Node::SharedPtr rosnode_{nullptr};
+  std::thread thread_executor_spin_;
+  rclcpp::executors::MultiThreadedExecutor::SharedPtr executor_;
+  bool stop_{false};
+  rclcpp::Publisher<buoy_msgs::msg::SCRecord>::SharedPtr sc_pub_;
+  buoy_gazebo::SpringState spring_state_;
+};
+
+//////////////////////////////////////////////////
+SpringController::SpringController()
+: dataPtr(std::make_unique<SpringControllerPrivate>())
+{
+}
+
+SpringController::~SpringController()
+{
+  // Stop controller manager thread
+  this->dataPtr->stop_ = true;
+  // this->dataPtr->executor_->remove_node(this->dataPtr->rosnode_);
+  this->dataPtr->executor_->cancel();
+  this->dataPtr->thread_executor_spin_.join();
+}
+
+void SpringController::Configure(const ignition::gazebo::Entity &_entity,
+    const std::shared_ptr<const sdf::Element> &_sdf,
+    ignition::gazebo::EntityComponentManager &_ecm,
+    ignition::gazebo::EventManager &)
+{
+  // Make sure the controller is attached to a valid model
+  const auto model = ignition::gazebo::Model(_entity);
+  if (!model.Valid(_ecm))
+  {
+    // TODO(andermi) this should not use rclcpp logging
+    // RCLCPP_ERROR(
+    //  this->dataPtr->rosnode_->get_logger(),
+    //  "[ROS 2 Spring Control] Failed to initialize because [%s] (Entity=%lu)] is not a model."
+    //  "Please make sure that ROS 2 Spring Control is attached to a valid model.",
+    //  model.Name(_ecm).c_str(), _entity);
+    return;
+  }
+  
+  this->dataPtr->entity_ = _entity;
+
+  // controller scoped name
+  auto scoped_name = ignition::gazebo::scopedName(this->dataPtr->entity_, _ecm, "/", false);
+
+  // ROS node
+  std::vector<std::string> arguments = {"--ros-args"};
+  auto ns = _sdf->Get<std::string>("namespace", scoped_name).first;
+  if (ns.empty() || ns[0] != '/')
+  {
+    ns = '/' + ns;
+  }
+  std::string ns_arg = std::string("__ns:=") + ns;
+  arguments.push_back(RCL_REMAP_FLAG);
+  arguments.push_back(ns_arg);
+  std::vector<const char *> argv;
+  for (const auto & arg : arguments)
+  {
+    argv.push_back(reinterpret_cast<const char *>(arg.data()));
+  }
+  if (!rclcpp::ok())
+  {
+    rclcpp::init(static_cast<int>(argv.size()), argv.data());
+    std::string node_name = "spring_controller";
+    this->dataPtr->rosnode_ = rclcpp::Node::make_shared(node_name);
+  }
+  this->dataPtr->executor_ = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  this->dataPtr->executor_->add_node(this->dataPtr->rosnode_);
+  this->dataPtr->stop_ = false;
+  auto spin = [this]()
+  {
+    while (rclcpp::ok() && !this->dataPtr->stop_) {
+      this->dataPtr->executor_->spin_once();
+    }
+  };
+  this->dataPtr->thread_executor_spin_ = std::thread(spin);
+
+  RCLCPP_DEBUG_STREAM(
+    this->dataPtr->node_->get_logger(), "[ROS 2 Spring Control] Setting up controller for [" <<
+      model.Name(_ecm) << "] (Entity=" << _entity << ")].");
+
+  // Publisher
+  auto topic = _sdf->Get<std::string>("topic", "sc_record").first;
+  this->dataPtr->sc_pub_ = this->dataPtr->node_->create_publisher<buoy_msgs::msg::SCRecord>(topic, 10);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+void SpringController::PostUpdate(const ignition::gazebo::UpdateInfo &_info,
+    const ignition::gazebo::EntityComponentManager &_ecm)
+{
+  this->dataPtr->current_time_ = _info.simTime;
+  if (this->dataPtr->sc_pub_.getNumSubscribers() <= 0 || _height == 0 || _width == 0)
+    return;
+  
+  auto sec_nsec = ignition::math::durationToSecNsec(_info.simTime);
+  
+  // Create new component for this entitiy in ECM (if it doesn't already exist)
+  this->dataPtr->spring_state_ = _ecm.Component<buoy_gazebo::SpringState>(this->dataPtr->entity_);
+  if (this->dataPtr->spring_state_ == nullptr)
+    // Pneumatic Spring hasn't updated values yet
+    return;
+  
+  buoy_msgs::msg::SCRecord sc_record;
+  sc_record.header.stamp.sec = sec_nsec.first;
+  sc_record.header.stamp.nsec = sec_nsec.second;
+  sc_record.seq_num = this->dataPtr->seq_num++;
+  sc_record.load_cell = this->dataPtr->spring_state_->load_cell;
+  sc_record.range_finder = this->dataPtr->spring_state_->range_finder;
+  sc_record.upper_psi = this->dataPtr->spring_state_->upper_psi;
+  sc_record.lower_psi = this->dataPtr->spring_state_->lower_psi;
+  // sc_record.epoch = this->dataPtr->epoch_;
+  // sc_record.salinity = this->dataPtr->salinity_;
+  // sc_record.temperature = this->dataPtr->temperature_;
+  // sc_record.status = this->dataPtr->status_;
+
+  this->dataPtr->sc_pub_.publish(sc_record);
+}
+
+
+
