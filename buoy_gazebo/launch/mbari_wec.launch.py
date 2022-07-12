2c7fa59137b551bd111dd5cf6ba93d2a2cde152a
Michael Anderson <anderson@mbari.org>
2022-07-12 12:41:01 -0700

    flake
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_gazebo/launch/mbari_wec.launch.py
+++ buoy_gazebo/launch/mbari_wec.launch.py
@@ -21,8 +21,8 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
