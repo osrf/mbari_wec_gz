1b6a5d21469fe04b68e673a763390a619aee94d0
Michael Anderson <anderson@mbari.org>
2022-07-12 15:46:28 -0700

    oops
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_tests/launch/no_inputs_py.launch.py
+++ buoy_tests/launch/no_inputs_py.launch.py
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-<<<<<<< HEAD
-import time
-=======
->>>>>>> main
 import unittest
 
 from buoy_msgs.interface import Interface
