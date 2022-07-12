72890cf88182f0aaaa6fb2546a340ca3d6d5ddec
Michael Anderson <anderson@mbari.org>
2022-07-11 17:25:47 -0700

    cant find cause of segfault at LinearInterpolator:91 starting from ElectroHydraulicSoln.hpp:115
    Signed-off-by: Michael Anderson <anderson@mbari.org>

---
+++ buoy_gazebo/launch/mbari_wec_debugger.launch.py
@@ -0,0 +1,49 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch Gazebo world with a buoy."""
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_buoy_gazebo = get_package_share_directory('buoy_gazebo')
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_buoy_gazebo, 'launch', 'debugger.launch.py'),
+        ),
+    )
+
+    bridge = Node(package='ros_ign_bridge',
+                  executable='parameter_bridge',
+                  arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+                  output='screen')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+          'ign_args',
+          default_value=[os.path.join(pkg_buoy_gazebo, 'worlds', 'mbari_wec.sdf'), ''],
+          description='Ignition Gazebo arguments'),
+        gazebo,
+        bridge
+    ])
