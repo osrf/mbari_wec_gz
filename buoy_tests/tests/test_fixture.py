301f00bdedb562c283cac67f96d477ba30124f48
Michael Anderson <anderson@mbari.org>
2022-07-20 15:03:50 -0700

    add minimal pytest for ignition.gazebo.TestFixture
    Signed-off-by: Michael Anderson <anderson@mbari.org>

---
+++ buoy_tests/tests/test_fixture.py
@@ -0,0 +1,86 @@
+# Copyright 2021 Open Source Robotics Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# If you compiled Ignition Gazebo from source you should modify your
+# `PYTHONPATH`:
+#
+# export PYTHONPATH=$PYTHONPATH:<path to ws>/install/lib/python
+#
+# Now you can run the example:
+#
+# python3 examples/scripts/python_api/helperFixture.py
+
+import os
+import unittest
+
+from ignition.common import set_verbosity
+from ignition.gazebo import TestFixture, World, world_entity
+
+
+class PyTestFixture(unittest.TestCase):
+
+    def setUp(self):
+        set_verbosity(4)
+
+        file_path = os.path.dirname(os.path.realpath(__file__))
+
+        self.helper = TestFixture(os.path.join(file_path, 'gravity.sdf'))
+
+        self.post_iterations = 0
+        self.iterations = 0
+        self.pre_iterations = 0
+        self.first_iteration = True
+
+        self.helper.on_post_update(self.on_post_udpate_cb)
+        self.helper.on_update(self.on_udpate_cb)
+        self.helper.on_pre_update(self.on_pre_udpate_cb)
+        self.helper.finalize()
+
+    def on_pre_udpate_cb(self, _info, _ecm):
+        self.pre_iterations += 1
+        if self.first_iteration:
+            self.first_iteration = False
+            world_e = world_entity(_ecm)
+            print('World entity is ', world_e)
+            w = World(world_e)
+            """
+            v = w.gravity(_ecm)
+            ^^ fails with:
+            [224.080s] 6: E           TypeError: Unable to convert function return value to a
+                                      Python type! The signature was
+            [224.080s] 6: E                 (self: ignition.gazebo.World,
+                                             arg0: ignition.gazebo.EntityComponentManager)
+                                            -> Optional[ignition::math::v6::Vector3<double>]
+            """
+            # print('Gravity ', v)
+            modelEntity = w.model_by_name(_ecm, 'falling')
+            print('Entity for falling model is: ', modelEntity)
+
+    def on_udpate_cb(self, _info, _ecm):
+        self.iterations += 1
+
+    def on_post_udpate_cb(self, _info, _ecm):
+        self.post_iterations += 1
+        if _info.sim_time.seconds == 1:
+            print('Post update sim time: ', _info.sim_time)
+
+    def test_server_run(self):
+        self.server = self.helper.server()
+        self.server.run(True, 1000, False)
+
+        print('iterations ', self.iterations)
+        print('post_iterations ', self.post_iterations)
+        print('pre_iterations ', self.pre_iterations)
+
+        self.assertEqual(self.iterations, 1000)
