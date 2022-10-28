a51b8ec6f86a0ae15b30491c76db7befaaa1d714
andermi <anderson@mbari.org>
2022-10-28 11:47:30 -0700

    Refine Spring (#102)
    * matched spring pretty well; need to do linters/tests
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * make spring pressure continuous across hysteresis
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * better fitting curves; almost there
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * fit model to test bench
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * test;linters
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * avoiding timeout from longer test
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * avoid timeout
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_gazebo/src/PolytropicPneumaticSpring/PolytropicPneumaticSpring.hpp
+++ buoy_gazebo/src/PolytropicPneumaticSpring/PolytropicPneumaticSpring.hpp
@@ -101,7 +101,8 @@ private:
     const int dt_nano,
     double & P1, const double & V1,
     double & P2, const double & V2);
-  void computeForce(const double & x, const double & v);
+  void computeLawOfCoolingForce(const double & x, const int & dt_nano);
+  void computePolytropicForce(const double & x, const double & v);
 
   ignition::transport::Node node;
   ignition::transport::Node::Publisher force_pub, pressure_pub, volume_pub,
