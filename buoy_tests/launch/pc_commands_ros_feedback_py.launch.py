b38a8c2c7e0168ab442c84bca12eeb98a437e90a
Michael Carroll <michael@openrobotics.org>
2022-11-29 16:05:53 -0600

    Lint
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- buoy_tests/launch/pc_commands_ros_feedback_py.launch.py
+++ buoy_tests/launch/pc_commands_ros_feedback_py.launch.py
@@ -62,8 +62,8 @@ class BuoyPCPyTest(BuoyPyTests):
     # Max amount to modify RPM in determining WindingCurrentLimit near ends of stroke
     MAX_RPM_ADJUSTMENT = 5000.0
 
-    def winding_current_limiter(self, I):
-        LimitedI = I
+    def winding_current_limiter(self, current):
+        LimitedI = current
         AdjustedN = self.node.rpm_
         RamPosition = (self.SC_RANGE_MAX - (self.node.range_finder_ / 0.0254))
         if self.node.rpm_ >= 0.0:  # Retracting
