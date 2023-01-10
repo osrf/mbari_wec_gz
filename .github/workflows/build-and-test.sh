ea2b1cc4b7d0cc8a84ea6734385fd6b4bb6fd344
Michael Anderson <anderson@mbari.org>
2023-01-10 10:04:19 -0800

    temp -- testing failed test
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -44,5 +44,6 @@ colcon build --packages-up-to buoy_tests --event-handlers console_direct+
 source $COLCON_WS/install/setup.bash
 
 # Test all buoy packages
-colcon test --packages-select-regex=buoy --packages-skip=buoy_msgs --event-handlers console_direct+
+#colcon test --packages-select-regex=buoy --packages-skip=buoy_msgs --event-handlers console_direct+
+launch_test $COLCON_WS/install/buoy_tests/share/buoy_tests/launch/pc_commands_ros_feedback_py.launch.py
 colcon test-result
