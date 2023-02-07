4b34bbcb9fecff8cf41161d47a278f00cca81c83
andermi <anderson@mbari.org>
2023-02-07 12:43:29 -0800

    update check for piston position after bias

--- buoy_tests/tests/pc_commands_ros_feedback.cpp
+++ buoy_tests/tests/pc_commands_ros_feedback.cpp
@@ -431,7 +431,7 @@ TEST_F(BuoyPCTests, PCCommandsInROSFeedback)
   EXPECT_GT(node->bias_curr_, bc - 0.1F);
   EXPECT_LT(node->bias_curr_, bc + 0.1F);
 
-  EXPECT_LT(node->range_finder_, 0.96);  // meters
+  EXPECT_LT(node->range_finder_, 0.97);  // meters
 
   // Let bias curr command timeout
   fixture->Server()->Run(
