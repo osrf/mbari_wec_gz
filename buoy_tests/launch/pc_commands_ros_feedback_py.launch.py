f102d3ecadb41e3542a9cd286f96fc126fa58284
Michael Anderson <anderson@mbari.org>
2023-02-07 12:47:06 -0800

    turn off rosbag for tests
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_tests/launch/pc_commands_ros_feedback_py.launch.py
+++ buoy_tests/launch/pc_commands_ros_feedback_py.launch.py
@@ -27,8 +27,7 @@ from testing_utils import default_generate_test_description
 
 
 def generate_test_description():
-    return default_generate_test_description(enable_rosbag=True,
-                                             rosbag_name='rosbag2_pc_cmds_py')
+    return default_generate_test_description()
 
 
 config = os.path.join(
--- buoy_tests/launch/sc_pump_ros_feedback_py.launch.py
+++ buoy_tests/launch/sc_pump_ros_feedback_py.launch.py
@@ -22,8 +22,7 @@ from testing_utils import default_generate_test_description
 
 
 def generate_test_description():
-    return default_generate_test_description(enable_rosbag=True,
-                                             rosbag_name='rosbag2_sc_pump_py')
+    return default_generate_test_description()
 
 
 class BuoySCPumpPyTest(BuoyPyTests):
