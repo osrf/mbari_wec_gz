a781eb4ad87b0db64979092efbae668f95f2a4a1
Mabel Zhang <mabel@openrobotics.org>
2023-06-16 22:43:07 -0400

    copyright year
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- buoy_gazebo/src/MooringForce/MooringForce.hpp
+++ buoy_gazebo/src/MooringForce/MooringForce.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Open Source Robotics Foundation, Inc.
+// Copyright 2023 Open Source Robotics Foundation, Inc.
 //                and Monterey Bay Aquarium Research Institute
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
