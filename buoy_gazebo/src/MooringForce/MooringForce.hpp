a4938fb36a7b54b494114950d36116c0154849df
Mabel Zhang <mabel@openrobotics.org>
2023-06-20 19:59:50 -0400

    Add mooring (#107)
    * mooring adaptation from tether
    * add seafloor and chain lying on it
    * add mooring test
    * remove SDF changes for mooring
    * add catenary solver (not working yet)
    * Fix math for y_B, return value of CatenaryHSoln. Fix comments
    * port mooring to Garden
    * fix math from PR feedback
    * compute V and H using live buoy position
    * revert unneeded changes in buoy_description/models/mbari_wec_base/model.sdf.em
    * do not evaluate at b = 0. Change anchor position
    * Fix Fx and Fy to Fr and Fz. Add effectiveRadius.
    * start solver loop at b=1 instead of b=0. Add sanity check
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>
    
    * update solver with current values each preupdate; modify solver while loop; minor tweaks
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    
    * update mooring test
    * ign -> gz
    * minor printout fix
    * tweak xtol and maxfev for RTF and passing test
    * remove inner solver for performance
    * profile mooring solver loop
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>
    
    * MooringForce: used closed form expression for catenary H position.
    - Update CatenaryHSoln with closed form for horizontal position and remove CatenaryVSoln.
    - Update MooringForce::PreUpdate removing while loop and using new initial estimate.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * MooringForce: additional debug info.
    - Add throttle for publishing debug info.
    - Add param for chain length.
    - Add error message if no valid links.
    - Notify when inside the effective radius.
    - Add check the force is finite.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * MooringForce: fix debug print statement
    Accept review feedback.
    Co-authored-by: Mabel Zhang <mabel.m.zhang@gmail.com>
    
    * Mooring Force: opening braces on new line
    - Enforce code standards.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * Mooring Force: format operators
    Accept review feedback.
    Co-authored-by: Mabel Zhang <mabel.m.zhang@gmail.com>
    
    * Mooring Force: more description message on solver fail
    Co-authored-by: Mabel Zhang <mabel.m.zhang@gmail.com>
    
    * Mooring Force: force buffer flush on gzmsg streams
    Accept review feedback.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * Mooring Force: update code style and doc strings
    A double underscore is reserved.
    A double underscore is reserved.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * Mooring Force: remove unused variables
    - Heave cone is not used, the mooring should attach to a single point.
    - Attachment point is hardcoded to buoy - this should change to a parameter.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * Mooring Force: rename buoyLink to link
    - The attachment point may not be the buoy (may be a bridle, heave cone,...).
    - Remove redundant variables.
    - Move hardcoded name to string linkName (set by param in follow up).
    - Apply code standards.
    - If link is not found in config it will not be found, so don't keep checking.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    * add toleranceL per srmainwaring, replaces effectiveRadius
    * fix merge glitch
    * revert braces and header guards to ROS styling
    * decrease force and iterations so mooring test doesnt go out of bounds
    * add SDF parameter to enable plugin
    * skip test
    * Add enable service. Fix enable SDF parsing. Call service from test.
    * change printout to warn
    * remove seafloor (no longer needed) because anchor is virtual
    * linter
    * increase mooring test timeout
    * copyright year
    * cpplint
    * copyright one line
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>
    
    ---------
    
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>
    Signed-off-by: Michael Anderson <anderson@mbari.org>
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    Co-authored-by: Michael Anderson <anderson@mbari.org>
    Co-authored-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ buoy_gazebo/src/MooringForce/MooringForce.hpp
@@ -0,0 +1,55 @@
+// Copyright 2023 Open Source Robotics Foundation, Inc. and Monterey Bay Aquarium Research Institute
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MOORINGFORCE__MOORINGFORCE_HPP_
+#define MOORINGFORCE__MOORINGFORCE_HPP_
+
+#include <memory>
+
+#include <gz/sim/System.hh>
+
+namespace buoy_gazebo
+{
+struct MooringForcePrivate;
+
+class MooringForce : public gz::sim::System,
+  public gz::sim::ISystemConfigure,
+  public gz::sim::ISystemPreUpdate
+{
+public:
+  /// \brief Constructor
+  MooringForce();
+
+  /// \brief Destructor
+  ~MooringForce() override;
+
+  // Documentation inherited
+  void Configure(
+    const gz::sim::Entity & _entity,
+    const std::shared_ptr<const sdf::Element> & _sdf,
+    gz::sim::EntityComponentManager & _ecm,
+    gz::sim::EventManager & _eventMgr) override;
+
+  // Documentation inherited
+  void PreUpdate(
+    const gz::sim::UpdateInfo & _info,
+    gz::sim::EntityComponentManager & _ecm) override;
+
+private:
+  /// \brief Private data pointer
+  std::unique_ptr<MooringForcePrivate> dataPtr;
+};
+}  // namespace buoy_gazebo
+
+#endif  // MOORINGFORCE__MOORINGFORCE_HPP_
