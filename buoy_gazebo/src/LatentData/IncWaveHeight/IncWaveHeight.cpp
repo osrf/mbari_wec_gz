c23d46a30130fa484ee57245e93e388b5352f307
Michael Anderson <anderson@mbari.org>
2025-08-12 18:01:57 -0700

    update inc wave height service
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- buoy_gazebo/src/LatentData/IncWaveHeight/IncWaveHeight.cpp
+++ buoy_gazebo/src/LatentData/IncWaveHeight/IncWaveHeight.cpp
@@ -143,7 +143,8 @@ struct IncWaveHeightPrivate
         std::unique_lock data(data_mutex_);
         next.unlock();
 
-        response->valid = this->inc_wave_valid_;
+        response->result.value =
+          this->inc_wave_valid_ ? response->result.OK : response->result.INVALID_INC_WAVE;
         if (!this->inc_wave_valid_) {
           data.unlock();
           return;
