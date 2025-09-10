69b315de57d120e3718b98fcada128a3f526b9d4
Michael Anderson <anderson@mbari.org>
2025-09-10 15:17:20 -0700

    update branch references to v2.0.0 tag
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -29,7 +29,7 @@ apt install -y git \
 
 cd $COLCON_WS_SRC
 cp -r $GITHUB_WORKSPACE $COLCON_WS_SRC
-wget https://raw.githubusercontent.com/osrf/mbari_wec/release/v2.0.0/mbari_wec_all.yaml
+wget https://raw.githubusercontent.com/osrf/mbari_wec/v2.0.0/mbari_wec_all.yaml
 vcs import --skip-existing < mbari_wec_all.yaml
 
 rosdep init
